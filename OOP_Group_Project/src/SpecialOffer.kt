e423b693a6849d943a5586be62bebf371b2fc704
AndreiMungiu <0munga41@solent.ac.uk>
2025-11-26 14:14:46 +0000

    updated classes

--- OOP_Group_Project/src/SpecialOffer.kt
+++ OOP_Group_Project/src/SpecialOffer.kt
@@ -1,3 +1,3 @@
 import java.time.LocalDate
 
-class SpecialOffer(val station: Station, val discountPercentage: Double, val startDate: LocalDate, val endDate: LocalDate)
+class SpecialOffer(val offerName: String, val station: Station, val discountPercentage: Double, val startDate: LocalDate, val endDate: LocalDate)
