6ca35dc79a214dd8f4ad0b6c0ce0fe2a29e5b383
Daniel-Vasile Anitas <danielanitas17@gmail.com>
2025-11-26 21:57:43 +0000

    member c special offer work

--- OOP_Group_Project/src/SpecialOffer.kt
+++ OOP_Group_Project/src/SpecialOffer.kt
@@ -1,3 +1,13 @@
-import java.time.LocalDate
+package OOP_Group_Project
 
-class SpecialOffer(val offerName: String, val station: Station, val discountPercentage: Double, val startDate: LocalDate, val endDate: LocalDate)
+class SpecialOffer(
+    val offerName: String,
+    val station: Station,
+    val discount: Double,
+    val description: String
+) {
+
+    fun showOfferDetails(): String {
+        return "Offer name: $offerName, Station: ${station.name}, Discount: $discount%, Info: $description"
+    }
+}
